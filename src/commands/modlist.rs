8417e35ca0ca1fdd90ee68730bb9b58029dbce35
Cor Peters <C.Peters@Eekels.com>
2023-09-20 14:12:40 +0200

    initial conflict detection

--- src/commands/modlist.rs
+++ src/commands/modlist.rs
@@ -99,8 +99,9 @@ pub fn list_mods(cache_dir: &Path) -> Result<()> {
 }
 
 pub fn show_mod(cache_dir: &Path, mod_name: &str) -> Result<()> {
-    if let Some(m) = find_mod(cache_dir, mod_name)? {
-        show_mod_status(&m);
+    let mod_list = gather_mods(cache_dir)?;
+    if let Some(m) = find_mod(&mod_list, mod_name)? {
+        show_mod_status(&m, &mod_list);
     } else {
         println!("No mod found by that name: {}", mod_name);
     }
@@ -108,32 +109,31 @@ pub fn show_mod(cache_dir: &Path, mod_name: &str) -> Result<()> {
     Ok(())
 }
 
-pub fn find_mod(cache_dir: &Path, mod_name: &str) -> Result<Option<Manifest>> {
-    if let Some(m) = find_mod_by_name(cache_dir, &mod_name)? {
+pub fn find_mod(mod_list: &[Manifest], mod_name: &str) -> Result<Option<Manifest>> {
+    if let Some(m) = find_mod_by_name(mod_list, &mod_name)? {
         Ok(Some(m))
     } else if let Ok(idx) = usize::from_str_radix(&mod_name, 10) {
-        find_mod_by_index(cache_dir, idx)
-    } else if let Some(m) = find_mod_by_name_fuzzy(cache_dir, &mod_name)? {
+        find_mod_by_index(mod_list, idx)
+    } else if let Some(m) = find_mod_by_name_fuzzy(mod_list, &mod_name)? {
         Ok(Some(m))
     } else {
         Ok(None)
     }
 }
 
-pub fn find_mod_by_index(cache_dir: &Path, idx: usize) -> Result<Option<Manifest>> {
-    Ok(gather_mods(cache_dir)?.get(idx).map(|m| m.clone()))
+pub fn find_mod_by_index(mod_list: &[Manifest], idx: usize) -> Result<Option<Manifest>> {
+    Ok(mod_list.get(idx).map(|m| m.clone()))
 }
-pub fn find_mod_by_name(cache_dir: &Path, name: &str) -> Result<Option<Manifest>> {
-    Ok(gather_mods(cache_dir)?
+pub fn find_mod_by_name(mod_list: &[Manifest], name: &str) -> Result<Option<Manifest>> {
+    Ok(mod_list
         .iter()
         .find_map(|m| (m.name() == name).then(|| m.clone())))
 }
-pub fn find_mod_by_name_fuzzy(cache_dir: &Path, fuzzy_name: &str) -> Result<Option<Manifest>> {
+pub fn find_mod_by_name_fuzzy(mod_list: &[Manifest], fuzzy_name: &str) -> Result<Option<Manifest>> {
     let matcher = SkimMatcherV2::default();
     let mut match_vec = Vec::new();
 
-    let mods = gather_mods(cache_dir)?;
-    mods.iter().for_each(|m| {
+    mod_list.iter().for_each(|m| {
         let i = matcher.fuzzy_match(m.name(), &fuzzy_name).unwrap_or(0);
         match_vec.push((m, i));
     });
@@ -145,7 +145,9 @@ pub fn find_mod_by_name_fuzzy(cache_dir: &Path, fuzzy_name: &str) -> Result<Opti
 
 //TODO: fancier printing
 //TODO move this to manifest Display
-pub fn show_mod_status(manifest: &Manifest) {
+pub fn show_mod_status(manifest: &Manifest, mod_list: &[Manifest]) -> Result<()> {
+    let conflict_list = conflict_list_by_mod(&mod_list)?;
+
     let mut table = Table::new();
     table
         .load_preset(NOTHING)
@@ -160,4 +162,6 @@ pub fn show_mod_status(manifest: &Manifest) {
         ]);
 
     println!("{table}");
+
+    Ok(())
 }
